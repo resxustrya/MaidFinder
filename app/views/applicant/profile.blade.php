f5dc6829c45661380e7069d518b17cd4f62fef16
jollyKuma <jollyrilakkuma@gmail.com>
2016-09-11 12:45:24 +0800

    some changes

--- app/views/applicant/profile.blade.php
+++ app/views/applicant/profile.blade.php
@@ -62,8 +62,8 @@
 
 
                         <div class="pmo-stat">
-                            <h2 class="m-0 c-white">1562</h2>
-                            Total Connections
+                            <h2 class="m-0 c-white">2</h2>
+                            Experience
                         </div>
                     </div>
 
@@ -81,51 +81,61 @@
                                 <i class="zmdi zmdi-pin"></i>
                                 <address class="m-b-0 ng-binding">
                                     {{ $location->location }}
+
                                 </address>
                             </li>
                         </ul>
                     </div>
 
                     <div class="pmo-block pmo-items hidden-xs">
-                        <h2>Connections</h2>
+                        <h2>Employers</h2>
 
                         <div class="pmob-body">
                             <div class="row">
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/1.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/1.jpg')}}" alt="">
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/2.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/2.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/3.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/3.jpg')}}" alt="">
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/4.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/4.jpg')}}" alt="">
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/5.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/5.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/6.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/6.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/7.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/7.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/8.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/8.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/1.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/9.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/2.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/1.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/3.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/2.jpg')}}" alt="">
+
                                 </a>
                                 <a href="" class="col-xs-2">
-                                    <img class="img-circle" src="img/profile-pics/4.jpg" alt="">
+                                    <img class="img-circle" src="{{asset('public/template/img/profile-pics/3.jpg')}}" alt="">
+
                                 </a>
                             </div>
                         </div>
@@ -161,10 +171,7 @@
                         </div>
                         <div class="pmbb-body p-l-30">
                             <div class="pmbb-view">
-                                Sed eu est vulputate, fringilla ligula ac, maximus arcu. Donec sed felis vel
-                                magna mattis ornare ut non turpis. Sed id arcu elit. Sed nec sagittis tortor.
-                                Mauris ante urna, ornare sit amet mollis eu, aliquet ac ligula. Nullam dolor
-                                metus, suscipit ac imperdiet nec, consectetur sed ex. Sed cursus porttitor leo.
+                                I am from pakitsan an I am willing to work very hard.
                             </div>
 
                             <div class="pmbb-edit">
@@ -217,15 +224,25 @@
                                     <dt>Martial Status</dt>
                                     <dd>{{$app->civilstatus}}</dd>
                                 </dl>
+                                <dl class="dl-horizontal">
+                                    <dt> Nationality</dt>
+                                    <dd>{{$app->nationality}}</dd>
+                                </dl>
+                                <dl class="dl-horizontal">
+                                    <dt>Religion</dt>
+                                    <dd>{{$app->religion}}</dd>
+                                </dl>
                             </div>
-
+                            <form action="{{ asset('/applicant/profile/edit') }}" method="POST">
                             <div class="pmbb-edit">
                                 <dl class="dl-horizontal">
                                     <dt class="p-t-10">Full Name</dt>
                                     <dd>
                                         <div class="fg-line">
-                                            <input type="text" class="form-control"
-                                                   placeholder="eg. Mallinda Hollaway">
+                                            <input type="text" name= "fname" class="form-control"
+                                                   placeholder="eg.FirstName" value="{{ $app->fname }}">
+                                            <input type="text" name= "lname" class="form-control"
+                                                   placeholder="eg.LasttName" value="{{ $app->lname }}">
                                         </div>
 
                                     </dd>
@@ -234,11 +251,14 @@
                                     <dt class="p-t-10">Gender</dt>
                                     <dd>
                                         <div class="fg-line">
-                                            <select class="form-control">
-                                                <option>Male</option>
-                                                <option>Female</option>
-                                                <option>Other</option>
-                                            </select>
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <select class="chosen" name="gender">
+                                                        <option {{ (isset($app->gender) and $app->gender == "Male") ? 'selected' : ''  }} value="Male">Male</option>
+                                                        <option {{ (isset($app->gender) and $app->gender == "Female") ? 'selected' : ''  }} value="Female">Female</option>
+                                                    </select>
+                                                </div>
+                                            </div>
                                         </div>
                                     </dd>
                                 </dl>
@@ -246,8 +266,35 @@
                                     <dt class="p-t-10">Birthday</dt>
                                     <dd>
                                         <div class="dtp-container dropdown fg-line">
-                                            <input type='text' class="form-control date-picker"
-                                                   data-toggle="dropdown" placeholder="Click here...">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <select class="chosen" name="year">
+                                                        <option value="" selected disabled>Year</option>
+                                                        <?php $date = explode('-', $app->birth); $count = 1; ?>
+                                                        @for($i = date('Y');50 > $count++; $i--)
+                                                            <option {{ $date[0] == $i ? 'selected' : ''}} value="{{ $i }}"> {{ $i }}</option>
+                                                        @endfor
+                                                    </select>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <select class="chosen" name="month">
+                                                        <option value="" selected disabled>Month</option>
+                                                        <?php $month = array("January", "Febuary", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"); ?>
+                                                        @foreach($month as $key => $value)
+                                                            <option {{ $date[1] == $key ? 'selected' : ''}} value="{{ $key}}">{{ $value }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="col-sm-4">
+                                                    <select name="day"  class="chosen">
+                                                        <option value="" selected disabled>Day</option>
+                                                        @for($i = 1; $i <= 31; $i++)
+                                                            <option {{ $date[2] == $i ? 'selected' : '' }} value="{{ $i }}">{{ $i }}</option>
+                                                        @endfor
+                                                    </select>
+                                                </div>
+
+                                            </div>
                                         </div>
                                     </dd>
                                 </dl>
@@ -255,20 +302,41 @@
                                     <dt class="p-t-10">Martial Status</dt>
                                     <dd>
                                         <div class="fg-line">
-                                            <select class="form-control">
-                                                <option>Single</option>
-                                                <option>Married</option>
-                                                <option>Other</option>
+                                            <select class="chosen" name="civilstatus">
+                                                <option disabled value="">Civil status</option>
+                                                <option disabled value="">Civil status</option>
+                                                <?php $status = array('Single', 'Married', 'Divorced', 'Widowed'); ?>
+                                                @foreach($status as $key => $value)
+                                                    <option {{ (isset($app->civilstatus) and $app->civilstatus == $key) ? 'selected' : '' }}  value="{{ $key }}">{{ $value }}</option>
+                                                @endforeach
                                             </select>
+                                            <span class="red-text">{{ isset($error) ? $error->first('civilstatus') : '' }}</span>
+                                        </div>
+                                    </dd>
+                                </dl>
+                                <dl class="dl-horizontal">
+                                    <dt> Nationality</dt>
+                                    <dd>
+                                        <div class="fg-line">
+                                            <input class="typeahead form-control" type="text" value="{{ $app->nationality != null ? $app->nationality : '' }}" name="nationality">
+                                        </div>
+                                    </dd>
+                                </dl>
+                                <dl class="dl-horizontal">
+                                    <dt>Religion</dt>
+                                    <dd>
+                                        <div class="fg-line">
+                                            <input class="typeahead form-control" type="text" value="{{ $app->religion != null ? $app->religion: '' }}" name="religion">
                                         </div>
                                     </dd>
                                 </dl>
 
                                 <div class="m-t-30">
-                                    <button class="btn btn-primary btn-sm">Save</button>
+                                    <button class="btn btn-primary btn-sm" type="submit" name="action">Save</button>
                                     <button data-ma-action="profile-edit-cancel" class="btn btn-link btn-sm">Cancel</button>
                                 </div>
                             </div>
+
                         </div>
                     </div>
 
@@ -307,7 +375,7 @@
                                     <dt class="p-t-10">Mobile Phone</dt>
                                     <dd>
                                         <div class="fg-line">
-                                            <input type="text" class="form-control"
+                                            <input type="text" class="form-control"value="{{ $app->contactno != null ? $app->contactno : '' }}" name="contactno"
                                                    placeholder="eg. 00971 12345678 9">
                                         </div>
                                     </dd>
@@ -322,31 +390,21 @@
                                     </dd>
                                 </dl>
                                 <dl class="dl-horizontal">
-                                    <dt class="p-t-10">Twitter</dt>
+                                    <dt class="p-t-10">Address</dt>
                                     <dd>
-                                        <div class="fg-line">
-                                            <input type="text" class="form-control" placeholder="eg. @malinda">
-                                        </div>
-                                    </dd>
-                                </dl>
-                                <dl class="dl-horizontal">
-                                    <dt class="p-t-10">Skype</dt>
-                                    <dd>
-                                        <div class="fg-line">
-                                            <input type="text" class="form-control"
-                                                   placeholder="eg. malinda.hollaway">
-                                        </div>
+                                        
                                     </dd>
                                 </dl>
 
                                 <div class="m-t-30">
-                                    <button class="btn btn-primary btn-sm">Save</button>
+                                    <button class="btn btn-primary btn-sm"type="submit" name="action">Save</button>
                                     <button data-ma-action="profile-edit-cancel" class="btn btn-link btn-sm">Cancel</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+               </form>
             </div>
         </div>
 
